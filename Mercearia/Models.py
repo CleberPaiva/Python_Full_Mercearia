dc69de555a01132d878cb1aa7f590cec22226c22
Cleber <paivadeap@gmail.com>
2022-02-21 17:12:46 -0300

    first commit

---
+++ Mercearia/Models.py
@@ -0,0 +1,12 @@
+class Categoria:
+    def __init__(self, categoria):
+        self.categoria = categoria
+
+class Produtos:
+    def __init__(self, nome, preco, categoria):
+        self.nome = nome
+        self.preco = preco
+        self.categoria = categoria
+
+
+    
