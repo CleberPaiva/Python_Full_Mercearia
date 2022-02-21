3413f80084d710f7db6f180e00b2958d1265d376
Cleber <paivadeap@gmail.com>
2022-02-21 19:40:59 -0300

    Terceiro commit

--- Mercearia/Models.py
+++ Mercearia/Models.py
@@ -16,7 +16,7 @@ class Estoque:
         self.quantidade = quantidade
 
 class Venda:
-    def __init__(self, itensVendido: Produtos, vendedor, comprador, quantidadeVendida, data = datetime.now()):
+    def __init__(self, itensVendido: Produtos, vendedor, comprador, quantidadeVendida, data = datetime.now().strftime("%d/%m/%y")):
         self.itensVendido = itensVendido
         self.vendedor = vendedor
         self.comprador = comprador
@@ -24,7 +24,7 @@ class Venda:
         self.data = data     
 
 class Fornecedor:
-    def __init__(self, nome, cnpj, telefone, email, endereco, categoria)
+    def __init__(self, nome, cnpj, telefone, email, endereco, categoria):
         self.nome = nome
         self.cnpj = cnpj
         self.telefone = telefone
